1cd424461f8874baddd555e364fc6bf41e115cf0
Leonid Levkovich <leonidlevkovich80@mail.ru>
2026-05-28 14:55:29 +0300

    feat: Лабораторная работа №3 - Moon Reports Frontend

--- lab6/public/components/backButton.js
+++
@@ -1,1 +0,0 @@
-export class BackButtonComponent { constructor(parent) { this.parent = parent; } render(onClick) { const btn = document.createElement('button'); btn.className = 'back-btn'; btn.textContent = '← Назад к списку'; btn.addEventListener('click', onClick); this.parent.appendChild(btn); } }
