9ea734e20278f41ee8cebddad816ef9a70242943
Leonid Levkovich <leonidlevkovich80@mail.ru>
2026-05-28 14:55:38 +0300

    feat: Лабораторная работа №5 - AJAX + XHR + CORS

--- lab6/backend/src/services/fileService.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const readData = (filePath) => { try { return JSON.parse(fs.readFileSync(filePath, 'utf8')); } catch (err) { console.error('Ошибка чтения:', err); return []; } };
-const writeData = (filePath, data) => { try { fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf8'); } catch (err) { console.error('Ошибка записи:', err); } };
-module.exports = { readData, writeData };
