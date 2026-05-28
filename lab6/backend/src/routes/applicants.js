d19630060246e5ac9e7a8a24df21cdac98e256c0
Leonid Levkovich <leonidlevkovich80@mail.ru>
2026-05-28 14:55:55 +0300

    chore: очистка main ветки, оставлен только README

--- lab6/backend/src/routes/applicants.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const applicantsController = require('../controllers/applicantsController');
-router.get('/', applicantsController.getAllApplicants);
-router.get('/:id', applicantsController.getApplicantById);
-router.post('/', applicantsController.createApplicant);
-router.patch('/:id', applicantsController.updateApplicant);
-router.delete('/:id', applicantsController.deleteApplicant);
-module.exports = router;
--- lab6/frontend/vite.config.js
+++
@@ -1,2 +0,0 @@
-import { defineConfig } from 'vite';
-export default defineConfig({ build: { outDir: './public', emptyOutDir: true, rollupOptions: { input: { main: 'index.html' } } }, server: { port: 5173 } });
